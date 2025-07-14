65d017c2c423fad6140d163e6cbdf6ebe0a16a6b
Gunjan Choukade <gunjanchoukade298@gmail.com>
2025-07-14 22:47:00 +0530

    changed file name5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,11 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview:{
+    port: 4173,
+    host: true,
+    allowedHosts: ['chatput-frontend.onrender.com'],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
