08e411f5af68ff3ffe31d854553e18389bc97e6c
Gunjan Choukade <gunjanchoukade298@gmail.com>
2025-07-14 21:59:35 +0530

    changed file name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'sonner'
-import SocketContext from './context/socketContext'
+import SocketContext from './context/SocketContext.jsx'
 import StoreContext from './context/StoreContext'
 
 
